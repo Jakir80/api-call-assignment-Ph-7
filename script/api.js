f5c4b011390d61d8dc5b92ee40653f60241952ef
Jakir80 <jh7593617@gmail.com>
2023-03-02 08:19:41 -1100

    create-modal-dynamik

--- script/api.js
+++ script/api.js
@@ -7,11 +7,11 @@ const loadFetch=()=>{
 }
 const showData=allData=>{
     // const {description,image,published_in,features,name,id}=alldata.data.tools;
-// console.log(allData.data.tools)
-const cardContainer=document.getElementById('card-container');
-
-allData.data.tools.forEach(singleData => {
-    console.log(singleData);
+    const cardContainer=document.getElementById('card-container');
+    // console.log(allData.data.id)
+    
+    allData.data.tools.forEach(singleData => {
+    // console.log(singleData.id);
 cardContainer.innerHTML+=`
 <div class="col">
 <div class="card h-100">
@@ -27,18 +27,67 @@ cardContainer.innerHTML+=`
   </div>
   <div class="card-footer">
    <h3>${singleData.name}</h3>
-   <div class="d-flex  justify-content: center;
-   align-items: center gap-2" >
-   <i class="fas fa-star"></i>
-   <p>${singleData.published_in}</p>
+   <div class="d-flex 
+ justify-content-between align-items-center  justify-content-center" >
+  <div class="d-flex align-items-center ">
+  <i class="fas fa-star"></i>
+  <p>${singleData.published_in}</p>
+  </div>
+  <i class="fas fa-arrow-right bg-warning rounded circle p-3 rounded" onclick="fetchDetails('${singleData.id}') "data-bs-toggle="modal" data-bs-target="#detailsModal"></i>
+   </div>       
+   </div>
    </div> 
   </div>
 </div>
-</div>  
 
+</div> 
 `;
 });
 
-}
+};  
 
+// modal js
+const fetchDetails = (id) => { 
+      const url =`https://openapi.programming-hero.com/api/ai/tool/${id}`
+     fetch(url)
+    .then(response => response.json())
+    .then(data => showDetails(data.data))
+    // console.log(url);
+    
+}
+const showDetails = (data) => {
+    console.log(data);
+    const {image_link, input_output_examples} = data;;
+    const modalBody = document.getElementById('modal-body');
+    modalBody.innerHTML = `
+    <div class="row">
+        <div class="col col-md-6">
+        <div class="col">
+        <div class="card h-100 p-2">
+          <img src="${image_link[0]}" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+        </div>
+        <div class="col col-md-6">
+        <div class="col">
+        <div class="card h-100 p-2">
+          <img src="${image_link[0]}" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <h5 class="card-title">${input_output_examples[0].input}</h5>
+            <p class="card-text">${input_output_examples[1].output}</p>
+          </div>
+        </div>
+      </div>
+        </div>
+    </div>
+    `
+};
 loadFetch();
+
+
+
+
