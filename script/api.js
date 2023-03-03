da298ee5eaf57207793d0f4de90f5864e5600181
Jakir80 <jh7593617@gmail.com>
2023-03-02 16:10:59 -1100

    complete-modal

--- script/api.js
+++ script/api.js
@@ -56,23 +56,56 @@ const fetchDetails = (id) => {
     
 }
 const showDetails = (data) => {
-    console.log(data);
-    const {image_link, input_output_examples} = data;;
+    console.log(data.integrations[0]);
+    // for (feature_name in data.features){
+    //   console.log(feature_name)
+    // }
+    const {image_link, input_output_examples,description} = data;
     const modalBody = document.getElementById('modal-body');
     modalBody.innerHTML = `
     <div class="row">
-        <div class="col col-md-6">
+        <div class="col col-md-7">
         <div class="col">
-        <div class="card h-100 p-2">
-          <img src="${image_link[0]}" class="card-img-top" alt="...">
+        <div class="card h-100 p-2 bg-warning-subtle">
+        <h1>${description}</h1>
+        <div class="d-flex gap-1">
+        <div class="bg-warning rounded circle p-2">
+        <h5 class="text-success">${data.pricing[0].price}<br> <br><span>${data.pricing[0].plan}</span></h5>
+        </div>
+        <div class="bg-warning rounded circle p-1">
+        <h5 class="text-danger-emphasis">${data.pricing[1].price} <br> <br>${data.pricing[1].plan}</h5>
+        </div>
+        <div class="bg-warning rounded circle p-1">
+        <h5 class="text-danger" >${data.pricing[2].price}<br><br>${data.pricing[2].plan}</h5>       
+        </div>      
+     </div>      
+<div class="d-flex gap-2 justify-content-between">
+  <div>
+    <h1>
+    features
+    </h1>
+<ul>
+    <li>${data.features["1"].feature_name}</li>
+    <li>${data.features["2"].feature_name}</li>
+    <li>${data.features["3"].feature_name}</li>
+</ul>
+</div>
+<div>
+    <h2>Intregation</h2>
+    <ul>
+        <li>${data.integrations[0]}</li>
+        <li>${data.integrations[1]}</li>
+        <li>${data.integrations[2]}</li>
+      </ul>
+    </div>
+      </div>
           <div class="card-body text-center">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <h5 class="card-title"></h5>            
           </div>
         </div>
       </div>
         </div>
-        <div class="col col-md-6">
+        <div class="col col-md-5">
         <div class="col">
         <div class="card h-100 p-2">
           <img src="${image_link[0]}" class="card-img-top" alt="...">
@@ -84,7 +117,7 @@ const showDetails = (data) => {
       </div>
         </div>
     </div>
-    `
+       `;
 };
 loadFetch();
 
